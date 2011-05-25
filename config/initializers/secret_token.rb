c192f29a2fef5f61a86b7be97afb8e8d046511c9
Quad Quadland <quad@quadland.com>
2011-05-25 19:24:57 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '597cac88731d3ed1ec49043ae02ff5d4a60440a7c7ef5904d453a28be180ca82bfdf45c2f6cfd548606b6da0e5ff1463b852fbd0f95e70b0fe701af73bbe999f'
